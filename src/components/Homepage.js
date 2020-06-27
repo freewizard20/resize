1a45fd60e38f7cd641d7578c78f2857d3ae972cb
fwcorp <gseongkyu79@gmail.com>
2020-06-27 22:46:17 +0900

    made two buttons

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -8,7 +8,10 @@ function Homepage() {
             <header>
                 <a href="http://localhost:3000"><img id="logo" src="logo.png" /></a>
             </header>
-
+            <section>
+                <button id="favorite-button" className="normal-button favorites">즐겨찾기에 추가</button>
+                <button id="home-button" className="normal-button favorites">홈 화면에 추가</button>
+            </section>
         </div>
     )
 }
