a2af9a16bee299daaa5882b085b1d93e95a69022
fwcorp <gseongkyu79@gmail.com>
2020-06-27 20:53:41 +0900

    adding new code

---
+++ src/components/Homepage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+// import { func1, func2 } from '../utils/functions';
+import './Homepage.css';
+
+function Homepage() {
+    return (
+        <div>
+            <img src="logo.png" />
+            <h1>This is homepage</h1>
+        </div>
+    )
+}
+
+export default Homepage;
