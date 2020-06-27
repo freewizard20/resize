f0d30d779cd65f2c63a726e0dfd360049f4c850b
fwcorp <gseongkyu79@gmail.com>
2020-06-27 23:02:27 +0900

    test deploy

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -6,7 +6,7 @@ function Homepage() {
     return (
         <div>
             <header>
-                <a href="http://localhost:3000"><img id="logo" src="logo.png" /></a>
+                {/* <a href="http://localhost:3000"><img id="logo" src="logo.png" /></a> */}
             </header>
             <section>
                 <button id="favorite-button" className="normal-button favorites">즐겨찾기에 추가</button>
