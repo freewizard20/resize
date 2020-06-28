9dd2322015ddbfe1be3aa79cbd739f350ef16204
fwcorp <gseongkyu79@gmail.com>
2020-06-28 19:55:02 +0900

    basic dropzone

--- src/components/Mainwork.js
+++ src/components/Mainwork.js
@@ -4,7 +4,7 @@ import './Mainwork.css'
 function Mainwork() {
     return (
         <div className="card main-work">
-
+            <form action="/file-upload" class="dropzone" id="my-awesome-dropzone"></form>
         </div>
     )
 }
