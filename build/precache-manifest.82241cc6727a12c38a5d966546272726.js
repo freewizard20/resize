d3fcdca739e3e675a01cb9c8b55bf6d6f5606dc2
fwcorp <gseongkyu79@gmail.com>
2020-06-28 11:55:35 +0900

    changing build strategy

---
+++ build/precache-manifest.82241cc6727a12c38a5d966546272726.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d573eba311e479b1265683158962596",
+    "url": "/index.html"
+  },
+  {
+    "revision": "539eabe98eeda5a7595b",
+    "url": "/static/css/main.9490788e.chunk.css"
+  },
+  {
+    "revision": "a1c2e4e88c2f9c4b05d9",
+    "url": "/static/js/2.7e0640fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7e0640fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "539eabe98eeda5a7595b",
+    "url": "/static/js/main.e399cd69.chunk.js"
+  },
+  {
+    "revision": "2340cde6fdc178b04456",
+    "url": "/static/js/runtime-main.9ecedec5.js"
+  }
+]);
