6e41034928db3eb5b3e9a6e239b55ef0b5bcebc1
fwcorp <gseongkyu79@gmail.com>
2020-07-08 21:55:35 +0900

    added about and tos

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 function About() {
-  return <div className="card middle static">This is About page</div>;
+  return <div className="card middle static">
+    <h1>About the app</h1>
+    <p className="static-content">이 웹앱은 모바일 및 데스크톱에 최적화되어 이미지 크기를 재조정할 수 있는 앱 입니다. 이미지 파일로는 png, jpg, jpeg, bmp, raw가 가능하며, 크기는 줄일 수도 있고, 늘릴 수도 있습니다. Javascript canvas 기능을 활용하여 구현되어서 96dpi로 출력합니다.
+    </p><p className="static-content">빠르고 직관적인 UX/UI를 구현하는 것을 목표로 앱을 만들었습니다. 불편한 부분이 있으시면 fw@fweasy.com 으로 메일 부탁드립니다! </p>
+    <p className="static-content"> 개발자 연락처 : fw@fweasy.com </p>
+  </div >;
 }
 
 export default About;
--- src/components/pages/Tos.js
+++ src/components/pages/Tos.js
@@ -1,7 +1,20 @@
 import React from "react";
 
 function Tos() {
-  return <div className="card static middle">This is tos page</div>;
+  return <div className="card static middle">
+    <h1>resize.fweasy.com 이용약관</h1>
+    <p>제 1조 ( 저작권 ) : resize.fweasy.com에 게시된 글들은 전적으로 제가 창조한 저작물이며, 무단으로 복사, 도용, 배포 등의 행위를 금지합니다. Creative Commons 2.0 라이센스를 기반으로 저작권을 명시하시고 저에게 활용 사실을 통보하시면 원하시는 곳으로 공유하실 수 있습니다.</p>
+    <p>제 2조 ( 개인정보 수집 대상 ) : resize.fweasy.com에서는 이메일을 포함한 어떠한 정보도 수집하지 않으며, Amazon Associates를 통한 Amazon.com 접속의 경우에 대해 태그 링크가 부여되는 것으로 알려져 있습니다.</p>
+    <p>제 3조 ( 공유에 관한 사항 ) : resize.fweasy.com는 방문자를 특정할 수 있는 개인정보를 별도로 수집 또는 공유하지 않습니다.</p>
+    <p>제 4조 ( 민감한 개인정보 ) : resize.fweasy.com는 민감한 개인정보에 대해 다음과 같은 대응 방안을 마련합니다.</p>
+    <ul>
+      <li >1. 민감한 개인정보는 본 resize.fweasy.com에서 요구하지도, 수집하지도, 활용하지도 않습니다.</li>
+      <li >2. 민감한 개인정보가 입수된 경우 즉시 파기하며, 기록이 남지 않도록 조치하고 개인정보를 입수하게 된 경위를 포함하여 해당 개인정보 소유자에게 즉시 통보합니다.</li>
+      <li >3. 민감한 개인정보를 어떠한 경우에도 요구하지 않는다는 사실을 명시합니다.</li>
+    </ul>
+    <p>제 5조 ( 개인 정보의 열람 ) : resize.fweasy.com는 어떠한 경우에도 개인정보를 요구, 보관, 활용하는 경우가 없도록 할 것이나 원하시는 경우 언제든지 본인의 개인정보에 관해 목록을 제공하거나, 즉시 파기하도록 하는 조치를 요구하고, 이에 따라 resize.fweasy.com는 즉시 해당 내용을 파기해야만 합니다.</p>
+    <p>제 6조 ( 개인정보의 수집 시기 ) : 저는 어떠한 시기에도 어떠한 방법으로도 어떠한 종류의 개인정보도 수집하지 않도록 합니다.</p>
+  </div>;
 }
 
 export default Tos;
